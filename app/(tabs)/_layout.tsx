dab8103e8445fb4112c14765b000bef5ab1e1cff
Ashish Gopal Hattimare <ashishgopalhattimare@gmail.com>
2025-09-04 23:42:08 +0530

    defalut

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
@@ -36,7 +36,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore 3',
+          title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
