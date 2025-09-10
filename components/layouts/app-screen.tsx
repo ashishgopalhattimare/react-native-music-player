c7b93b2c20f1d18f881c2dad35edf1b4f3d97fb2
Ashish Gopal Hattimare <ashishgopalhattimare@gmail.com>
2025-09-11 00:33:06 +0530

    update platform flow

--- components/layouts/app-screen.tsx
+++ components/layouts/app-screen.tsx
@@ -6,6 +6,7 @@ import { styles } from '@/styles/common';
 export const AppScreen = ({ children }: PropsWithChildren) => {
   return Platform.select({
     android: <View style={[styles.screen, { overflow: 'scroll' }]}>{children}</View>,
+    ios: <View style={[styles.screen, { overflow: 'scroll' }]}>{children}</View>,
     default: (
       <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.screen}>
         {children}
